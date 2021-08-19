70517f07f32800023b7a6e8fca26b5cc33bcb919
mrxrsd <mrxrsd@gmail.com>
2021-08-19 00:36:40 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# Akka.Discovery.Redis
-Akka.Discovery.Redis
+# Redis Discovery
+This module can be used as a discovery method for an Redis Discovery based cluster.
+
+To use `Akka.Discovery.Redis` in your project, you must also include `Akka.Discovery` in your project nuget package dependency.
+You will also need to include these HOCON settings in your HOCON configuration:
+```
+akka.discovery {
+  # Set the following in your application.conf if you want to use this discovery mechanism:
+  # method = redis
+  redis {
+    class = "Akka.Discovery.Redis, Akka.Discovery.Redis"
+
+    # connection string, as described here: https://stackexchange.github.io/StackExchange.Redis/Configuration#basic-configuration-strings
+    configuration-string = ""
+
+    # Redis discovery key. Leave it for default or change it to appropriate value. WARNING: don't change it on production instances.
+    discovery-key = ""
+    }    
+}
+
+```
---
+++ src/Akka.Discovery.Redis/RedisServiceDiscovery.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Akka.Actor;
+using StackExchange.Redis;
+
+namespace Akka.Discovery.Redis
+{
+
+    internal sealed class RedisServiceDiscovery : ServiceDiscovery
+    {
+        private readonly Configuration.Config _config;
+        private readonly string _discoveryKey;
+        private readonly string _connStr;
+        private bool _isClustered;
+
+        public RedisServiceDiscovery(ActorSystem system)
+        {
+            _config = system.Settings.Config.GetConfig("akka.discovery.redis");
+            _discoveryKey = _config.GetString("discovery-key", "akka-cluster-discovery");
+            _connStr = _config.GetString("configuration-string");
+        }
+
+
+        private IDatabase RedisClient
+        {
+
+            get
+            {
+                var redisConnection = ConnectionMultiplexer.Connect(_connStr);
+                _isClustered = IsClustered(redisConnection);
+                return redisConnection.GetDatabase();
+            }
+        }
+
+
+
+        // https://github.com/akkadotnet/Akka.Persistence.Redis/blob/dev/src/Akka.Persistence.Redis/RedisExtensions.cs
+        public static bool IsClustered(IConnectionMultiplexer connection)
+        {
+            return connection.GetEndPoints()
+                .Select(endPoint => connection.GetServer(endPoint))
+                .Any(server => server.ServerType == ServerType.Cluster);
+        }
+
+        public override async Task<Resolved> Lookup(Lookup lookup, TimeSpan resolveTimeout)
+        {
+            using (var cts = new CancellationTokenSource(resolveTimeout))
+            {
+                try
+                {
+                    return await Lookup(lookup, cts.Token);
+                }
+                catch (TaskCanceledException e)
+                {
+                    throw new TaskCanceledException($"Lookup for [{lookup}] timed out, within [{resolveTimeout}]", e);
+                }
+            }
+        }
+
+        private async Task<Resolved> Lookup(Lookup query, CancellationToken token)
+        {
+            var resolvedTargets = (await GetEntries()).Where(x=> x.ServiceName == query.ServiceName && IsValid(x))
+                .Select(x => new ResolvedTarget(x.Host,
+                                                x.Port,
+                                                IPAddress.TryParse(x.Host, out var ipAddress) ? ipAddress : null)
+                ).ToList();
+
+            return new Resolved(query.ServiceName, resolvedTargets);
+        }
+
+        private Task<IList<DiscoveryEntry>> GetEntries()
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool IsValid(DiscoveryEntry entry)
+        {
+            return true;
+        }
+    }
+
+
+    internal class DiscoveryEntry
+    {
+        public string ServiceName { get; set; }
+        public string Host { get; set; }
+        public int? Port { get; set; }
+        public DateTime LastHeartBeat { get; set; }
+    }
+}
