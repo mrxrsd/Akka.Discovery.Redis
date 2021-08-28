e56658eaa2c2fe757a3ed6af70d96fb3297d2b3f
mrxrsd <mrxrsd@gmail.com>
2021-08-28 18:34:04 -0300

    discovery

--- src/Akka.Discovery.Redis/RedisServiceDiscovery.cs
+++ src/Akka.Discovery.Redis/RedisServiceDiscovery.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Akka.Actor;
+using Akka.Event;
 using StackExchange.Redis;
 
 namespace Akka.Discovery.Redis
@@ -13,16 +14,22 @@ namespace Akka.Discovery.Redis
 
     internal sealed class RedisServiceDiscovery : ServiceDiscovery
     {
+        private readonly ActorSystem _system;
+        private readonly ILoggingAdapter _log;
         private readonly Configuration.Config _config;
         private readonly string _discoveryKey;
         private readonly string _connStr;
         private bool _isClustered;
+        private readonly TimeSpan _aliveTimeout;
 
         public RedisServiceDiscovery(ActorSystem system)
         {
+            _system = system;
+            _log = Logging.GetLogger(system, typeof(RedisServiceDiscovery));
             _config = system.Settings.Config.GetConfig("akka.discovery.redis");
             _discoveryKey = _config.GetString("discovery-key", "akka-cluster-discovery");
             _connStr = _config.GetString("configuration-string");
+            _aliveTimeout = _config.GetTimeSpan("alive-timeout", TimeSpan.FromSeconds(60));
         }
 
 
@@ -64,7 +71,7 @@ namespace Akka.Discovery.Redis
 
         private async Task<Resolved> Lookup(Lookup query, CancellationToken token)
         {
-            var resolvedTargets = (await GetEntries()).Where(x=> x.ServiceName == query.ServiceName && IsValid(x))
+            var resolvedTargets = (await GetEntries(query.ServiceName)).Where(x=> IsValid(x))
                 .Select(x => new ResolvedTarget(x.Host,
                                                 x.Port,
                                                 IPAddress.TryParse(x.Host, out var ipAddress) ? ipAddress : null)
@@ -73,21 +80,32 @@ namespace Akka.Discovery.Redis
             return new Resolved(query.ServiceName, resolvedTargets);
         }
 
-        private Task<IList<DiscoveryEntry>> GetEntries()
+        private async Task<IList<DiscoveryEntry>> GetEntries(string serviceName)
         {
-            throw new NotImplementedException();
+            return (await RedisClient.ListRangeAsync(GetRedisKey(serviceName))).Select(x =>
+                PersistentFromBytes<DiscoveryEntry>(x)).ToList();
+        }
+
+        private T PersistentFromBytes<T>(byte[] bytes)
+        {
+            var serializer = _system.Serialization.FindSerializerForType(typeof(T));
+            return serializer.FromBinary<T>(bytes);
+        }
+
+        private RedisKey GetRedisKey(string serviceName)
+        {
+            return $"akkaDiscovery:{_discoveryKey}:{serviceName}";
         }
 
         private bool IsValid(DiscoveryEntry entry)
         {
-            return true;
+            return DateTime.UtcNow.Subtract(entry.LastHeartBeat) > _aliveTimeout;
         }
     }
 
 
     internal class DiscoveryEntry
     {
-        public string ServiceName { get; set; }
         public string Host { get; set; }
         public int? Port { get; set; }
         public DateTime LastHeartBeat { get; set; }
---
+++ src/SampleApp/SampleActor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SampleActor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
